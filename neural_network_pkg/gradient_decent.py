8f39ad41641df739e631f8768514d73d18c17bed
cpprian <cpprian456@gmail.com>
2022-10-22 15:39:40 +0200

    fix returning error while training

--- neural_network_pkg/gradient_decent.py
+++ neural_network_pkg/gradient_decent.py
@@ -9,17 +9,28 @@ class GradientDecent:
         self.goal = goal
         self.w = w
         self.prediction = None
-        self.n = self.w.shape[0]
+        self.n = self.goal.shape[0]
+        self.err = 0
 
     def train(self, time):
         for _ in range(time):
+            err_era = 0
             for i in range(self.goal.shape[1]):
                 y_i = np.expand_dims(self.goal[:, i], axis=1)
-                self.w -= self.alpha * self.delta(self.x[:, i], y_i)
+                
+                d = self.delta(self.x[:, i], y_i)
+                err_era += self.error(y_i)
+                self.w -= self.alpha * d
+            self.err = err_era
 
     def delta(self, x_i, y_i, bias=0):
         self.prediction = neural_network(x_i, self.w, bias)
         return 2/self.n * np.outer(self.prediction - y_i, x_i)
 
-    def error(self):
-        return np.sum(np.square(self.prediction - self.goal)) / self.n
+    def error(self, y_i):
+        sum_prediction_goal = 0
+        for i in range(y_i.shape[0]):
+            sum_prediction_goal += np.square(self.prediction[i, 0] - y_i[i, 0])
+
+        return sum_prediction_goal / self.n
+
--- neural_network_pkg/tests/test_gradient_decent.py
+++ neural_network_pkg/tests/test_gradient_decent.py
@@ -3,7 +3,7 @@ from neural_network_pkg.gradient_decent import GradientDecent
 
 def test_one_neuron_prediction() -> None:
     alpha = 0.1
-    times = np.array([5, 15])
+    times = 5
     x = np.array([2], dtype=float, ndmin=2)
     goal = np.array([0.8], dtype=float, ndmin=2)
     w = np.array([0.5], dtype=float, ndmin=2)
@@ -11,7 +11,7 @@ def test_one_neuron_prediction() -> None:
     gd = GradientDecent(alpha, x, goal, w)
     gd.train(times[0])
     assert gd.prediction == 0.80032
-    assert np.round(gd.error(), 10) == 0.0000001024
+    assert np.round(gd.err, 10) == 0.0000001024
 
 
 def test_matrix_neuron_prediction() -> None:
@@ -21,7 +21,6 @@ def test_matrix_neuron_prediction() -> None:
         [0.5, 0.1, 0.2, 0.8], 
         [0.75, 0.3, 0.1, 0.9],
         [0.1, 0.7, 0.6, 0.2]], dtype=float, ndmin=2)
-        
         
     y = np.array([
         [0.1, 0.5, 0.1, 0.7],
@@ -40,5 +39,5 @@ def test_matrix_neuron_prediction() -> None:
 
     gd = GradientDecent(alpha, x, y, w)
     gd.train(times)
-    assert gd.error() == 0.258218
+    assert np.round(gd.err, 6) == 0.258218
 
