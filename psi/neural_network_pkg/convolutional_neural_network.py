c05e05f0f6e96d9fe8de95fa845a4c47caa0960d
cpprian <cpprian456@gmail.com>
2022-12-12 19:31:39 +0100

    init pool & unpool

--- psi/neural_network_pkg/convolutional_neural_network.py
+++ psi/neural_network_pkg/convolutional_neural_network.py
@@ -6,6 +6,7 @@ class ConvolutionalNeuralNetwork:
 
     def __init__(self) -> None:
         image_sections = None
+        copy_index = None
 
     def load_input(self, filename: str, number_of_input: int=1000) -> np.ndarray:
         with open(filename, "rb") as f:
@@ -78,14 +79,14 @@ class ConvolutionalNeuralNetwork:
                 
                 layer = self.convolve(input_batch, filters, 3, 3)
                 kernel_layer = layer.reshape(batch_size, -1)
-                kernel_layer = self.tanh(kernel_layer)
-                dropout_mask = np.random.randint(2, size=kernel_layer.shape)
-                kernel_layer *= dropout_mask
-                layer_output = self.softmax(np.dot(kernel_layer, wy))
+                kernel_layer = self.relu(kernel_layer)
+                # dropout_mask = np.random.randint(2, size=kernel_layer.shape)
+                # kernel_layer *= dropout_mask
+                layer_output = self.relu(np.dot(kernel_layer, wy))
 
                 layer_output_delta = 2 * 1/layer_output.shape[0] * (layer_output - labels[batch_start:batch_end])
                 kernel_layer_1_delta = wy @ layer_output_delta.T
-                kernel_layer_1_delta *= dropout_mask.T
+                # kernel_layer_1_delta *= dropout_mask.T
 
                 kernel_layer_1_delta = kernel_layer_1_delta * self.relu_deriv(kernel_layer.T)
                 kernel_layer_1_delta = kernel_layer_1_delta.reshape(kernel_layer.shape)
@@ -100,7 +101,7 @@ class ConvolutionalNeuralNetwork:
         return output_wy, output_filters
 
     def predict(self, input_image: np.ndarray, kernel: np.ndarray, wy: np.ndarray) -> np.ndarray:
-        kernel_layer = self.tanh(self.convolve(input_image, kernel, filter_height=3, filter_width=3).reshape(-1, 1))
+        kernel_layer = self.relu(self.convolve(input_image, kernel, filter_height=3, filter_width=3).reshape(-1, 1))
         return wy.T @ kernel_layer
 
     def accuracy(self, f_log: str, labels: np.ndarray, input_image: np.ndarray, filters: np.ndarray, wy: np.ndarray) -> float:
@@ -137,10 +138,44 @@ class ConvolutionalNeuralNetwork:
     def pool(self, input_image: np.ndarray, filter_size: int=2, stride: int=2) -> np.ndarray:
         input_image_height, input_image_width = input_image.shape
         output_image = np.zeros((int(input_image_height/stride), int(input_image_width/stride)))
+        self.copy_index = np.zeros((int(input_image_height/stride), int(input_image_width/stride)))
 
         for i in range(0, input_image_height, stride):
             for j in range(0, input_image_width, stride):
                 output_image[int(i/stride), int(j/stride)] = np.max(input_image[i:i+filter_size, j:j+filter_size])
+                self.copy_index[int(i/stride), int(j/stride)] = np.argmax(input_image[i:i+filter_size, j:j+filter_size])
 
         return output_image
-    
+
+    def pool2(self, input_image: np.ndarray, filter_size: int=2, stride: int=2) -> np.ndarray:
+        input_image_height, input_image_width = input_image.shape
+        output_image = np.zeros((int(input_image_height/stride), int(input_image_width/stride)))
+        self.copy_index = np.zeros((int(input_image_height/stride), int(input_image_width/stride)))
+
+        for i in range(0, input_image_height, stride):
+            for j in range(0, input_image_width, stride):
+                output_image[int(i/stride), int(j/stride)] = np.max(input_image[i:i+filter_size, j:j+filter_size])
+                self.copy_index[int(i/stride), int(j/stride)] = np.argmax(input_image[i:i+filter_size, j:j+filter_size])
+
+        return output_image
+
+    def unpool(self, input_image: np.ndarray, stride: int=2) -> np.ndarray:
+        output_image = np.zeros((input_image.shape[0]*2, input_image.shape[1]*2))
+        temp = np.zeros((stride, stride), dtype=float)
+
+        for i in range(input_image.shape[0]):
+            for j in range(input_image.shape[1]):
+                idx = self.copy_index[i, j]
+                if idx <= 1:
+                    temp_idx = (0, idx)
+                else:
+                    temp_idx = (1, idx-2)
+                
+                (y, x) = temp_idx
+                temp[y, int(x)] = input_image[i, j]
+                if (output_image[i:i+stride, j:j+stride].shape != (2, 2)):
+                    break
+                output_image[i:i+stride, j:j+stride] = temp
+                temp = np.zeros((stride, stride), dtype=float)
+
+        return output_image
